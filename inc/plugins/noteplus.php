d6f3327aea076e0ab9bfb86650fe904fd7896fe8
Unknown <volca780@gmail.com>
2017-12-30 14:49:38 +0100

    Function: info
    Cette fonction retourne les informations par rapport au plugin.

---
+++ inc/plugins/noteplus.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @function Return plugin information
+ * @return array
+ */
+function noteplus_info()
+{
+    return [
+        "name"          => "NotePlus",
+        "description"   => "Display note for all user.",
+        "author"        => "",
+        "authotsite"    => "",
+        "version"       => "1.0",
+        "compatibility" => "18*"
+    ];
+}
