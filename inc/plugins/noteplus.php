dae810a720d198919b4c758eb5aa76749c7d4c65
Unknown <volca780@gmail.com>
2017-12-30 15:04:10 +0100

    Function: install
    Installation des parametre sur le panneau administration.

--- inc/plugins/noteplus.php
+++ inc/plugins/noteplus.php
@@ -22,5 +22,39 @@ function noteplus_info()
  */
 function noteplus_install()
 {
-    
+    global $db;
+
+    $gid = $db->insert_query("settinggroups", [
+        "name"          => "noteplus_sg",
+        "title"         => "NotePlus",
+        "description"   => "Configuration of NotePlus",
+        "disporder"     => 1,
+        "isdefault"     => 0
+    ]);
+
+    $settings = [
+        "noteplus_enabled" => [
+            "title"         => "Enabled",
+            "description"   => "If the yes box is checked, the plugin will be activated.",
+            "optionscode"   => "yesno",
+            "value"         => 1,
+            "disporder"     => 1
+        ],
+        "noteplus_message" => [
+            "title"         => "Message",
+            "description"   => "The message to display on the homepage.",
+            "optionscode"   => "textarea",
+            "value"         => "My message :)",
+            "disporder"     => 2
+        ]
+    ];
+
+    foreach ($settings as $name => $setting) {
+        $setting["name"] = $name;
+        $setting["gid"]  = $gid;
+
+        $db->insert_query("settings", $setting);
+    }
+
+    rebuild_settings();
 }
